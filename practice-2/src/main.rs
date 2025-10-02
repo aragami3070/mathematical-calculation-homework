5dea77b8860cbc9dadd3e8ceb13d77d757417e0e
aragami3070 <smirnov17612@gmail.com>
2025-10-02 10:24:26 +0400

    Fix(practice-2): lagrange solve

--- practice-2/src/main.rs
+++ practice-2/src/main.rs
@@ -1,5 +1,6 @@
-use core::f64;
+use itertools::Itertools;
 use std::io;
+
 fn main() {
     let mut input = String::new();
     io::stdin()
@@ -19,18 +20,25 @@ fn main() {
         .map(|f| f.trim().parse::<f64>().expect("Failed parse f_list"))
         .collect();
 
+    let x_extended_list: Vec<f64> = x_list
+        .iter()
+        .tuple_windows()
+        .flat_map(|(x_0, x_1)| [*x_0, (x_0 + x_1) / 2.0])
+        .chain(x_list.last().copied())
+        .collect();
+
     let mut numerator = 1.0;
     let mut denominator = 1.0;
 
     let mut result: Vec<f64> = Vec::new();
 
-    for j in 0..x_list.len() {
+    for j in 0..x_extended_list.len() {
         let mut sum = 0.0;
         for k in 0..f_list.len() {
             let f_k = f_list[k];
             for i in 0..x_list.len() {
                 if k != i {
-                    numerator *= x_list[j] - x_list[i];
+                    numerator *= x_extended_list[j] - x_list[i];
                     denominator *= x_list[k] - x_list[i];
                 }
             }
